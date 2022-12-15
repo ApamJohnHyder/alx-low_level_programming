30bbb326881a065ee54683974d5dbad5b7b7ee35
apamjohnhyder <johnapam4@gmail.com>
2022-12-15 09:57:21 -0800

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _puchar(char c);
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,5 +14,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
 
 #endif
