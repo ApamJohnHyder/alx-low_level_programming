39332373742230ea33ced65a7b8c0c5125e94d04
apamjohnhyder <johnapam4@gmail.com>
2022-12-15 02:21:10 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if parameter is an uppercase letter.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
+ */
+{
+	if (c >= 65 && c <= 90)
+	{
+		Return: (1);
+	}
+	else
+	{
+		Return: (0);
+	}
+}
