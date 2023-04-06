c918f52147d24d73cc94eb036068c1e9d105f51e
apamjohnhyder <johnapam4@gmail.com>
2023-04-06 07:51:21 -0700

    >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+
+hash_table_create - creates a hash table
+
+@size: size of the array
+
+Return: pointer to the newly created hash table, or NULL on failure
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+if (size == 0)
+return (NULL);
+
+hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+if (hash_table == NULL)
+return (NULL);
+
+hash_table->size = size;
+hash_table->array = calloc(size, sizeof(hash_node_t *));
+if (hash_table->array == NULL)
+{
+free(hash_table);
+return (NULL);
+}
+
+return (hash_table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This is the README.md file for 0x1A-hash_tables
